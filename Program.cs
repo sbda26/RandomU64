7bb54715f07c000c1b1c8d6920066b547e159980
sbda26 <46138243+sbda26@users.noreply.github.com>
2022-01-07 12:52:29 -0500

    Replaced Hungarian prefix notation.

--- Program.cs
+++ Program.cs
@@ -2,13 +2,16 @@
 using System.Collections.Generic; // only necessary for List<double> _lstMilliseconds
 using System.Linq; // only necessary for PrintSortedTotals()
 
+
+// HOW TO GENERATE A 64-BIT RANDOM INTEGER USING 6 DIFFERENT METHODS AND COMPARING THE TIME LAPSES FOR EACH.
+
 namespace RandomU64
 {
     class Program
     {
-        static private Random _clsRandom = new Random();
+        static private Random _random = new Random();
 
-        private const int _ciIterations = 100000;
+        private const int ITERATIONS = 100000;
 
         private struct TotalsStructure
         {
@@ -16,23 +19,23 @@ namespace RandomU64
             public double TotalMilliseconds;
         }
 
-        private static List<TotalsStructure> _lstMillisecondTotals = new List<TotalsStructure>();
+        private static List<TotalsStructure> millisecondTotalsList = new List<TotalsStructure>();
 
 
         static void Main(string[] args)
         {
             Func<ulong>[] arr_Methods = { Method1, Method2, Method3, Method4, Method5, Method6 };
 
-            Console.WriteLine(string.Format("{0} iterations for each method.", _ciIterations));
+            Console.WriteLine(string.Format("{0} iterations for each method.", ITERATIONS));
 
             foreach(Func<ulong> methodX in arr_Methods)
             {
-                string sMethodName = methodX.Method.Name;
-                double dTotalMilliseconds = RunMethod(methodX);
+                string MethodName = methodX.Method.Name;
+                double totalMilliseconds = RunMethod(methodX);
 
-                PrintHeader(sMethodName);
-                PrintFooter(dTotalMilliseconds);
-                AddToStatsList(sMethodName, dTotalMilliseconds);
+                PrintHeader(MethodName);
+                PrintFooter(totalMilliseconds);
+                AddToStatsList(MethodName, totalMilliseconds);
             }    
 
             PrintStats();
@@ -41,44 +44,42 @@ namespace RandomU64
 
         static double RunMethod(Func<ulong> MethodX)
         {
-            DateTime dtStart = DateTime.Now;
+            DateTime startTime = DateTime.Now;
 
-            for (int iCount = 1; iCount <= _ciIterations; iCount++)
+            for (int count = 1; count <= ITERATIONS; count++)
                 _ = MethodX.Invoke();
 
-            return (DateTime.Now - dtStart).TotalMilliseconds;
+            return (DateTime.Now - startTime).TotalMilliseconds;
         }
 
 
-        static void PrintHeader(string sMethodName)
+        static void PrintHeader(string methodName)
         {
             Console.WriteLine("--------------------------------------------");
-            Console.WriteLine(string.Format("Running {0}()", sMethodName));
+            Console.WriteLine(string.Format("Running {0}()", methodName));
         }
 
-        static void PrintFooter(double dTotalMilliseconds)
-        {
-            Console.WriteLine(string.Format("Elapsed time: {0} milliseconds", dTotalMilliseconds));
-        }
+        static void PrintFooter(double totalMilliseconds) =>
+            Console.WriteLine(string.Format("Elapsed time: {0} milliseconds", totalMilliseconds));
 
-        static void AddToStatsList(string sMethodName, double dTotalMilliseconds)
+        static void AddToStatsList(string methodName, double totalMilliseconds)
         {
-            int iMethodNumber = Convert.ToInt32(sMethodName.Substring(sMethodName.Length - 1, 1));
-            _lstMillisecondTotals.Add(new TotalsStructure { MethodNumber = iMethodNumber, TotalMilliseconds = dTotalMilliseconds });
+            int methodNumber = Convert.ToInt32(methodName.Substring(methodName.Length - 1, 1));
+            millisecondTotalsList.Add(new TotalsStructure { MethodNumber = methodNumber, TotalMilliseconds = totalMilliseconds });
         }
 
         static void PrintStats()
         {
-            int[] arr_iLengths = _lstMillisecondTotals.OrderBy(s => s.TotalMilliseconds).Select(n => n.MethodNumber).ToArray();
-            int iLimit = arr_iLengths.GetUpperBound(0);
+            int[] lengths = millisecondTotalsList.OrderBy(s => s.TotalMilliseconds).Select(n => n.MethodNumber).ToArray();
+            int limit = lengths.GetUpperBound(0);
 
             Console.WriteLine();
             Console.Write("Methods in order of speed (fastest -> slowest): ");
             
-            for (int iIndex = 0; iIndex <= iLimit; iIndex++)
+            for (int index = 0; index <= limit; index++)
             {
-                Console.Write(arr_iLengths[iIndex]);
-                if (iIndex < iLimit)
+                Console.Write(lengths[index]);
+                if (index < limit)
                     Console.Write(", ");
                 else
                     Console.WriteLine();
@@ -90,13 +91,14 @@ namespace RandomU64
 
         static ulong Method1()
         {
-            ulong ulResult = 0;
+            ulong result = 0;
 
-            for (int iIndex = 0; iIndex < 32; iIndex++)
-                ulResult += Convert.ToUInt64((uint)_clsRandom.Next(int.MinValue, int.MaxValue));
+            for (int index = 0; index < 32; index++)
+                result += Convert.ToUInt64((uint)_random.Next(int.MinValue, int.MaxValue));
 
-            return ulResult;
+            return result;
         }
+
         static ulong Method2()
         {
             /*
@@ -107,8 +109,8 @@ namespace RandomU64
              * 5) y = y OR x2
             */
 
-            int x1 = _clsRandom.Next(int.MinValue, int.MaxValue);
-            int x2 = _clsRandom.Next(int.MinValue, int.MaxValue);
+            int x1 = _random.Next(int.MinValue, int.MaxValue);
+            int x2 = _random.Next(int.MinValue, int.MaxValue);
             ulong y;
 
             // lines must be separated or result won't go past 2^32
@@ -122,49 +124,49 @@ namespace RandomU64
         static ulong Method3()
         {
             /*
-             * 1) ulResult = 0
+             * 1) result = 0
              * 2) Loop power 0->63
-             * 3)   dRandom = random floating point value between 0->1
-             * 4)   if dRandom < 0.5
+             * 3)   random = random floating point value between 0->1
+             * 4)   if random < 0.5
              * 5)       dValue = 2 ^ power
              * 6)       ulValue = dValue converted to unsigned 64-bit
-             * 7)       ulResult = ulResult OR ulValue
+             * 7)       result = result OR ulValue
              * 8)  End Loop
             */
   
-            ulong ulResult = 0;
+            ulong result = 0;
 
-            for(int iPower = 0; iPower < 64; iPower++)
+            for(int power = 0; power < 64; power++)
             {
-                double dRandom = _clsRandom.NextDouble();
-                if(dRandom > 0.5)
+                double random = _random.NextDouble();
+                if(random > 0.5)
                 {
-                    double dValue = Math.Pow(2, iPower);
+                    double dValue = Math.Pow(2, power);
                     ulong ulValue = Convert.ToUInt64(dValue);
-                    ulResult = ulResult | ulValue;
+                    result = result | ulValue;
                 }
             }
 
-            return ulResult;
+            return result;
         }
 
         static ulong Method4()  // only difference between #4 and #3 is that this one (#4) uses .Next() instead of .NextDouble()
         {
             /*
-             * 1) ulResult = 0
+             * 1) result = 0
              * 2) Loop power 0->63
              * 3)   if random 0 or 1 = 1
-             * 4)       ulResult = ulResult OR ConvertTo64bit(2 ^ power)
+             * 4)       result = result OR ConvertTo64bit(2 ^ power)
              * 5) End Loop
             */
 
-            ulong ulResult = 0;
+            ulong result = 0;
 
-            for (int iPower = 0; iPower < 64; iPower++)
-                if (_clsRandom.Next(0, 1) == 1)
-                    ulResult = ulResult | Convert.ToUInt64(Math.Pow(2, iPower));
+            for (int power = 0; power < 64; power++)
+                if (_random.Next(0, 1) == 1)
+                    result = result | Convert.ToUInt64(Math.Pow(2, power));
 
-            return ulResult;
+            return result;
         }
 
         static ulong Method5()
@@ -176,11 +178,11 @@ namespace RandomU64
             */
             
             byte[] arr_bt = new byte[8];
-            ulong ulResult;
+            ulong result;
 
-            _clsRandom.NextBytes(arr_bt);
-            ulResult = BitConverter.ToUInt64(arr_bt, 0);
-            return ulResult;
+            _random.NextBytes(arr_bt);
+            result = BitConverter.ToUInt64(arr_bt, 0);
+            return result;
         }
 
         // Next method courtesy of https://stackoverflow.com/questions/14708778/how-to-convert-unsigned-integer-to-signed-integer-without-overflowexception/39107847
@@ -197,21 +199,21 @@ namespace RandomU64
              * evil.Int32 and evil.UInt32 occupy same space in memory (aka unions). So, any change to one immediately changes the other
              * 
              * 1) Get signed 32-bit random integer evil.Int32 between -(2^31 + 1) and +(2^31 - 1)
-             * 2) ulResult = evil.UInt32
-             * 3) Shift ulResult value 32 bits to left
+             * 2) result = evil.UInt32
+             * 3) Shift result value 32 bits to left
              * 4) Get new signed 32-bit random integer value into evil.Int32 between -(2^31 + 1) and +(2^31 - 1)
-             * 5) ulResult = ulResult | evil.UInt32
+             * 5) result = result | evil.UInt32
             */
             var evil = new EvilUnion();
-            ulong ulResult = 0;
+            ulong result = 0;
 
-            evil.Int32 = _clsRandom.Next(int.MinValue, int.MaxValue);
-            ulResult = evil.UInt32;
-            ulResult = ulResult << 32;
-            evil.Int32 = _clsRandom.Next(int.MinValue, int.MaxValue);
-            ulResult = ulResult | evil.UInt32;
+            evil.Int32 = _random.Next(int.MinValue, int.MaxValue);
+            result = evil.UInt32;
+            result = result << 32;
+            evil.Int32 = _random.Next(int.MinValue, int.MaxValue);
+            result = result | evil.UInt32;
 
-            return ulResult;
+            return result;
         }
     }
 }
